2e4b64d53ac7d85c11bc9f81d40989b83e2575a9
kolamor <kolamorev@mail.ru>
2020-07-21 02:08:08 +0300

    create tcp_connector

--- src/app.py
+++ src/app.py
@@ -21,14 +21,30 @@ async def create_app(config: dict) -> aiohttp.web.Application:
 
 async def on_start(app):
     config = app['config']
+    session = aiohttp.ClientSession(connector=create_tcp_connector(config))
     # app['db'] = await asyncpgsa.create_pool(dsn=config['database_uri'])
     # app['http_client'] = ClientSession(connector=TCPConnector(limit=config['http_client_limit_TCPConnector'],
     #                                                           limit_per_host=config['http_client_limit_per_host_TCPConnector']
     #                                                           ))
 
 async def on_shutdown(app):
-    # logger.info('on_shutdown')
+    logger.info('on_shutdown')
     # await app['db'].close()
     # logger.info('PSQL closed')
     await app['http_client'].close()
     logger.info('http_client closed')
+
+
+def create_tcp_connector(config: dict) -> TCPConnector:
+    """
+    :param config: dict
+
+    :return: TCPConnector
+    """
+    connector = TCPConnector(
+        limit_per_host=config.get('TCP_limit_per_host', 100),
+        limit=config.get('TCP_limit_per_host', 100),
+        verify_ssl=config.get('verify_ssl', True),
+        **config
+    )
+    return connector
