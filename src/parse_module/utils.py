c0ac7412bf73c21d9a7a9b52f1da75798c8fb70f
tt <you@e.com>
2020-07-23 22:44:29 +0300

    parser, re_ip

---
+++ src/parse_module/utils.py
@@ -0,0 +1,10 @@
+import re
+
+IPPattern = re.compile(
+    r'(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)'
+)
+
+IPPortPatternLine = re.compile(
+    r'^.*?(?P<ip>(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)).*?(?P<port>\d{2,5}).*$',  # noqa
+    flags=re.MULTILINE,
+)
--- tests/modules_test.py
+++ tests/modules_test.py
@@ -2,7 +2,7 @@ import pytest
 from aiohttp import ClientSession, TCPConnector
 from src.app import create_tcp_connector
 from src.parse_module import request_get, DefaultParse
-
+from src.parse_module.utils import IPPortPatternLine
 
 @pytest.mark.asyncio
 async def test_create_tcp_connector():
@@ -17,9 +17,31 @@ async def aiohttp_session():
     pass
 
 
+urls = ('https://google.com', 'http://httpbin.org/')
+
+
+@pytest.mark.parametrize('url', urls)
 @pytest.mark.asyncio
-async def test_request_get(aiohttp_session):
-    url = 'https://google.com'
+async def test_request_get(url, aiohttp_session):
     text = await request_get(session=aiohttp_session, url=url)
     assert isinstance(text, str)
     assert len(text) > 0
+
+
+proxy_templates = [('''https://wert:ioopp@192.134.65.88:9000
+                    http://23.11.67.100:80
+                    socks4://89.78.13.10:44890
+                    socks5://67.105.188.1:5001''',
+                    ('192.134.65.88', '9000'), ('23.11.67.100', '80'), ('89.78.13.10', '44890'), ('67.105.188.1', '5001'))
+                   ]
+
+
+@pytest.mark.parametrize('template',  proxy_templates)
+@pytest.mark.regexp
+def test_parse_ip(template):
+    temp, *res = template
+    pars = IPPortPatternLine.findall(temp)
+
+    for r in res:
+        assert r in pars
+
