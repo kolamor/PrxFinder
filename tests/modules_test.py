f855557833683c8c3a6367d97ea3eef0d18edee3
tt <you@e.com>
2020-07-25 02:15:36 +0300

    .

--- tests/modules_test.py
+++ tests/modules_test.py
@@ -6,6 +6,7 @@ from src.parse_module import request_get, DefaultParse
 from src.parse_module.utils import IPPortPatternLine
 from src import ProxyClient, Proxy
 
+
 @pytest.mark.asyncio
 async def test_create_tcp_connector():
     tcp_conn = create_tcp_connector({})
@@ -48,8 +49,8 @@ def test_parse_ip(template):
         assert r in pars
 
 
-
 proxy_list = ['socks5://138.197.2.106:56658', 'http://login:passs@37.45.89.1:4890']
+
 
 class TestProxy:
 
@@ -61,7 +62,7 @@ class TestProxy:
 
 class TestClient:
 
-    @pytest.mark.parametrize('proxy', proxy_list )
+    @pytest.mark.parametrize('proxy', proxy_list)
     @pytest.mark.client
     @pytest.mark.asyncio
     async def test_proxy_client(self, proxy):
@@ -69,6 +70,6 @@ class TestClient:
         async with ProxyClient(proxy=proxy) as sess:
             assert isinstance(sess._session, aiohttp.ClientSession)
             assert sess.closed is False
-        assert sess._session.closed is True
+        assert sess.closed is True
 
 
